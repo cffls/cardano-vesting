7f579d1d13f381932212326807c5f3933b98f1df
Jerry <jerrycgh@gmail.com>
2022-08-28 20:37:03 -0700

    Parameterize contract

--- plutus/src/Vesting.hs
+++ plutus/src/Vesting.hs
@@ -15,6 +15,9 @@
 
 module Main where
 
+import System.Environment
+
+import           Data.String (IsString(fromString))
 import           Control.Monad        hiding (fmap)
 
 import qualified Data.ByteString.Lazy           as LBS
@@ -34,7 +37,9 @@ import qualified Plutus.V2.Ledger.Contexts as PSU.V2
 
 import qualified Plutus.V2.Ledger.Api           as PlutusV2
 
-import           Prelude              (IO)
+import           Prelude              (IO, putStrLn)
+import qualified Ledger.Ada as PlutusV2
+import Plutus.V1.Ledger.Bytes (fromHex)
 
 
 data VestingDatum = VestingDatum
@@ -42,12 +47,16 @@ data VestingDatum = VestingDatum
     , deadline    :: PlutusV2.POSIXTime
     }
 
+vestingFee :: Integer
+vestingFee = 1500000
+
 PlutusTx.unstableMakeIsData ''VestingDatum
 
 {-# INLINABLE mkValidator #-}
-mkValidator :: VestingDatum -> () -> PlutusV2.ScriptContext -> Bool
-mkValidator dat () ctx = traceIfFalse "beneficiary's signature missing" signedByBeneficiary &&
-                         traceIfFalse "deadline not reached" deadlineReached
+mkValidator :: PubKeyHash -> VestingDatum -> () -> PlutusV2.ScriptContext -> Bool
+mkValidator pkh dat () ctx = traceIfFalse "beneficiary's signature missing" signedByBeneficiary &&
+                         traceIfFalse "deadline not reached" deadlineReached &&
+                         traceIfFalse "insufficient fee" paidFee
   where
     info :: PlutusV2.TxInfo
     info = PlutusV2.scriptContextTxInfo ctx
@@ -58,21 +67,36 @@ mkValidator dat () ctx = traceIfFalse "beneficiary's signature missing" signedBy
     deadlineReached :: Bool
     deadlineReached = contains (from $ deadline dat) $ PSU.V2.txInfoValidRange info
 
+    paidFee :: Bool
+    paidFee = PlutusV2.getLovelace (PlutusV2.fromValue (PSU.V2.valuePaidTo info pkh)) >= vestingFee
 
-validator :: PlutusV2.Validator
-validator = PlutusV2.mkValidatorScript
+
+validator :: PubKeyHash -> PlutusV2.Validator
+validator pkh = PlutusV2.mkValidatorScript $
     $$(PlutusTx.compile [|| wrap ||])
- where
-    wrap = PSU.V2.mkUntypedValidator mkValidator
+    `PlutusTx.applyCode`
+    PlutusTx.liftCode pkh
+    where
+        wrap = PSU.V2.mkUntypedValidator . mkValidator
+        
 
 
-scriptSBSV2 :: SBS.ShortByteString
-scriptSBSV2 = SBS.toShort . LBS.toStrict $ serialise validator
+scriptSBSV2 :: PubKeyHash -> SBS.ShortByteString
+scriptSBSV2 pkh = SBS.toShort . LBS.toStrict $ serialise $ validator pkh
 
 
-serialisedScriptV2 :: PlutusScript PlutusScriptV2
-serialisedScriptV2 = PlutusScriptSerialised scriptSBSV2
+serialisedScriptV2 :: PubKeyHash -> PlutusScript PlutusScriptV2
+serialisedScriptV2 pkh = PlutusScriptSerialised $ scriptSBSV2 pkh
 
 main :: IO ()
 main = do
-    void $ writeFileTextEnvelope "vesting-plutusV2.plutus" Nothing serialisedScriptV2
+    args <- getArgs
+    if null args
+        then do
+            putStrLn "Please provide a public key hash to which the fee will be sent!"
+        else
+            let pkh = fromHex . fromString . head $ args
+            in case pkh of
+                Left err -> putStrLn $ "Invalid public key hash: " ++ err
+                Right (PlutusV2.LedgerBytes bytes) -> do
+                    void $ writeFileTextEnvelope "vesting-plutusV2.plutus" Nothing $ serialisedScriptV2 $ PubKeyHash bytes
