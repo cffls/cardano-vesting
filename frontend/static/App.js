118197b61cf8f9e3d669ce57976bba9da0b60b60
Jerry <jerrycgh@gmail.com>
2022-10-17 00:30:37 -0700

    Change frontend UI

--- frontend/static/App.js
+++ frontend/static/App.js
@@ -10,8 +10,23 @@ class RecipientList extends React.Component {
         <RecipientListItem key={index} item={item} index={index} removeItem={this.props.removeItem}/>
       );
     });
+
     return (
-      <ul className="list-group"> {items} </ul>
+      <table className="table">
+        <thead>
+          <tr>
+            <th style={{width: "5%"}} scope="col">#</th>
+            <th style={{width: "40%"}} scope="col">Recipient</th>
+            <th style={{width: "20%"}} scope="col">Vest Amount</th>
+            <th style={{width: "25%"}} scope="col">Vest date</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+        {items}
+        <RecipientForm items={this.props.items} addItem={this.props.addItem} />
+        </tbody>
+      </table>
     );
   }
 }
@@ -21,19 +36,23 @@ class RecipientListItem extends React.Component {
     super(props);
     this.onClickClose = this.onClickClose.bind(this);
   }
+
   onClickClose() {
     var index = parseInt(this.props.index);
     this.props.removeItem(index);
   }
+
   render () {
-    var recipientClass = "undone"
     return(
-      <li className="list-group-item ">
-        <div className={recipientClass}>
-          {this.props.item.value.newAddressValue} --- {this.props.item.value.newAmountValue}
-          <button type="button" className="close" onClick={this.onClickClose}>&times;</button>
-        </div>
-      </li>
+      <tr>
+        <th scope="row">{this.props.index+1}</th>
+        <td>{this.props.item.value.newAddressValue}</td>
+        <td>{this.props.item.value.newAmountValue}</td>
+        <td>{this.props.item.value.newDeadlineValue}</td>
+        <td >
+          <button className="btn btn-sm btn-danger" type="button" onClick={this.onClickClose}>&times;</button>
+        </td>
+      </tr>
     );
   }
 }
@@ -43,39 +62,45 @@ class RecipientForm extends React.Component {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
   }
-  componentDidMount() {
-    this.refs.address.focus();
-    this.refs.amount.focus();
-  }
+
   onSubmit(event) {
     event.preventDefault();
     var newAddressValue = this.refs.address.value;
     var newAmountValue = this.refs.amount.value;
+    var newDeadlineValue = this.refs.deadline.value;
 
     if(newAddressValue) {
-      console.log(newAddressValue);
-      this.props.addItem({newAddressValue, newAmountValue});
+      this.props.addItem({newAddressValue, newAmountValue, newDeadlineValue});
+      this.refs.address.value = "";
+      this.refs.amount.value = "";
+      this.refs.deadline.value = "";
       this.refs.form.reset();
     }
   }
   render () {
     return (
-      <form ref="form" onSubmit={this.onSubmit} className="form-inline">
-        <input type="text" ref="address" className="form-control" placeholder="add a testnet address..."/>
-        <input type="text" ref="amount" className="form-control" placeholder="amount in ada"/>
-        <button type="submit" className="btn btn-default">Add</button>
-      </form>
+      <tr>
+        <th scope="row">{this.props.items.length+1}</th>
+        <td>
+          <input type="text" className="form-control" ref="address" placeholder="Cardano address"/>
+        </td>
+        <td>
+          <input type="text" className="form-control" ref="amount" placeholder="Vest amount"/>
+        </td>
+        <td>
+          <input type="date" className="form-control" ref="deadline" placeholder="Vest date"/>
+        </td>
+        <td>
+          <form ref="form" onSubmit={this.onSubmit} className="form-inline">
+            <button type="submit" className="btn btn-sm btn-primary">Add</button>
+          </form>
+        </td>
+      </tr>
     );
   }
 }
 
-class RecipientHeader extends React.Component {
-  render () {
-    return <h1>ADA Vest</h1>;
-  }
-}
-
-class RecipientApp extends React.Component {
+class CreateVestApp extends React.Component {
   constructor (props) {
     super(props);
     this.onChange = this.onChange.bind(this);
@@ -180,17 +205,16 @@ class RecipientApp extends React.Component {
   }
   render() {
     return (
-      <div id="main">
-        <RecipientHeader />
-        <RecipientList items={this.props.initItems} removeItem={this.removeItem}/>
-        <RecipientForm addItem={this.addItem} />
-        <br/>
-        <button disabled={!this.state.connected} onChange={this.onChange} onClick={this.prepare_sender}>Submit Tx</button>
-        <button disabled={this.state.connected} onChange={this.onChange} onClick={this.connectWallet}>Connect Nami wallet</button>
+      <div className="main card">
+        <div className="card-body">
+            <RecipientList items={this.props.initItems} addItem={this.addItem} removeItem={this.removeItem}/>
+            <button id="submitGrants" className="btn btn-primary" disabled={!this.state.connected} onChange={this.onChange} onClick={this.prepare_sender}>Submit Tx</button>
+            {/*<button className="btn btn-primary" disabled={this.state.connected} onChange={this.onChange} onClick={this.connectWallet}>Connect Nami wallet</button>*/}
+        </div>
       </div>
     );
   }
 }
 
-ReactDOM.render(<RecipientApp initItems={recipientItems}/>, document.getElementById('app'));
+ReactDOM.render(<CreateVestApp initItems={recipientItems}/>, document.getElementById('app'));
 
